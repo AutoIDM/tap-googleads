bcf96f666c513040a771632fbf551a78d0c95685
Josh Lloyd <jlloyd@ip-10-0-0-10.ec2.internal>
2022-07-12 12:56:23 -0600

    fixed 400 bad path error

--- tap_googleads/client.py
+++ tap_googleads/client.py
@@ -20,7 +20,7 @@ SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 class GoogleAdsStream(RESTStream):
     """GoogleAds stream class."""
 
-    url_base = "https://googleads.googleapis.com/v8"
+    url_base = "https://googleads.googleapis.com/v11"
 
     records_jsonpath = "$[*]"  # Or override `parse_response`.
     next_page_token_jsonpath = "$.nextPageToken"  # Or override `get_next_page_token`.
